0db97388ea20e089c17bdc880ff1b159695be3d2
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-06-03 20:34:12 -0500

    Se agregan archivos de librerias y se importan en el index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD PHONE - PHP VUE</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./public/libs/bootstrap/css/bootstrap.min.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="./public/libs/fontawesome/css/all.min.css">
+    <!-- SweetAlert2 CSS -->
+    <link rel="stylesheet" href="./public/libs/sweetalert2/css/sweetalert2.min.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="./public/css/style.css">
+</head>
+<body>
+    <h1>crudphone</h1>
+
+    <!-- jQuery, Popper JS, Bootstrap JS -->
+    <script src="./public/libs/jquery/jquery-3.6.0.min.js"></script>
+    <script src="./public/libs/popperjs/popper.min.js"></script>
+    <script src="./public/libs/bootstrap/js/bootstrap.min.js"></script>
+    <!-- SweetAlert2 JS -->
+    <script src="./public/libs/sweetalert2/js/sweetalert2.all.min.js"></script>
+    <!-- Vue JS -->
+    <script src="./public/libs/vue/vue.min.js"></script>
+    <!-- Axios JS -->
+    <script src="./public/libs/axios/axios.min.js"></script>
+    <!-- Main -->
+    <script src="./public/js/main.js"></script>
+</body>
+</html>
