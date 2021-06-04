27db8897beb2c357415f404dadfe03d7538f3de6
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-06-04 08:19:38 -0500

    Se hace metodo del boton editar btnUpdate en vuejs

--- public/js/main.js
+++ public/js/main.js
@@ -47,8 +47,9 @@ var app = new Vue({
               
               if (this.brand == '' || this.model == '' || this.stock == 0) {
                 Swal.fire({
-                    type: 'info',
-                    title: 'Datos completos',
+                    title: 'Datos incompletos',
+                    icon: 'info',
+                    confirmButtonColor: '#3085d6'
                 });
               }
               else {
@@ -60,12 +61,51 @@ var app = new Vue({
                       timer: 3000
                   });
                   Toast.fire({
-                      typre: 'success',
-                      title: 'Producto agregado!'
+                      title: 'Producto agregado!',
+                      icon: 'success'
                   });
               }
         },
-        btnUpdate: async function(id, marca, modelo, stock) {},
+        btnUpdate: async function(id, marca, modelo, stock) {
+            const { value: formValues } = await Swal.fire({
+                title: 'EDITAR',
+                html:
+                    '<div class="row">' +
+                        '<label for="" class="col-sm-3 col-form-label">Marca</label>' +
+                        '<div class="col-sm-7">' +
+                            '<input type="text" id="brand" class="form-control" value="'+brand+'">' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="row">' +
+                        '<label for="" class="col-sm-3 col-form-label">Modelo</label>' +
+                        '<div class="col-sm-7">' +
+                            '<input type="text" id="model" class="form-control" value="'+model+'">' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="row">' +
+                        '<label for="" class="col-sm-3 col-form-label">Stock</label>' +
+                        '<div class="col-sm-7">' +
+                            '<input type="number" id="stock" class="form-control" min="0" value="'+stock+'">' +
+                        '</div>' +
+                    '</div>',
+                focusConfirm: false,
+                showCancelButton: true,
+                }).then((result) => {
+                    if (result) {
+                        brand = document.getElementById('brand').value,
+                        model = document.getElementById('model').value,
+                        stock = document.getElementById('stock').value,
+                        
+                        this.updatePhone(id, marca, modelo, stock);
+                        Swal.fire({
+                            title: '!Actualizado!',
+                            text: '!El registro ha sido actualizado!',
+                            icon: 'success',
+                            confirmButtonColor: '#3085d6',
+                        });
+                    }
+                });
+        },
         btnDelete: async function(id) {},
         
     },
