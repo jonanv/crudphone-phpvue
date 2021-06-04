d38e1275b1c4e3e26ad4f9aa8a3f88c672cfff88
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-06-04 09:50:48 -0500

    Se hacen los metodos crear, modificar y eliminar en vuejs

--- public/js/main.js
+++ public/js/main.js
@@ -5,12 +5,11 @@ var app = new Vue({
         phones: [],
         marca: '',
         model: '',
-        stock: '',
-        total: 0,
-        totalStock: 0
+        stock: 0,
+        total: 0
     },
     methods: {
-        // Botones
+        // BOTONES
         btnCreate: async function() {
             const { value: formValues } = await Swal.fire({
                 title: 'NUEVO',
@@ -68,7 +67,7 @@ var app = new Vue({
                   });
               }
         },
-        btnUpdate: async function(id, marca, modelo, stock) {
+        btnUpdate: async function(id, brand, model, stock) {
             const { value: formValues } = await Swal.fire({
                 title: 'EDITAR',
                 html:
@@ -92,14 +91,15 @@ var app = new Vue({
                     '</div>',
                 focusConfirm: false,
                 showCancelButton: true,
+                confirmButtonColor: '#3085d6',
             })
             .then((result) => {
-                    if (result) {
+                    if (result.value) {
                         brand = document.getElementById('brand').value,
                         model = document.getElementById('model').value,
                         stock = document.getElementById('stock').value,
                         
-                        this.updatePhone(id, marca, modelo, stock);
+                        this.updatePhone(id, brand, model, stock);
                         Swal.fire({
                             title: '!Actualizado!',
                             text: '!El registro ha sido actualizado!',
@@ -130,17 +130,53 @@ var app = new Vue({
                     }
                 });
         },
-        // Procesimientos
+        // PROCESIMIENTOS
+        // Listar
         listPhones: function() {
             axios.post(url, {option: 4})
                 .then(response => {
                     console.log(response);
                     this.phones = response.data;
                 });
+        },
+        // Crear
+        createPhone: function() {
+            axios.post(url, {option: 1, brand:this.brand, model:this.model, stock:this.stock})
+                .then(response => {
+                    console.log(response);
+                    this.listPhones();
+                });
+                this.brand = '';
+                this.model = '';
+                this.stock = 0;
+        },
+        // Editar
+        updatePhone: function(id, brand, model, stock) {
+            axios.post(url, {option: 2, id:id, brand:brand, model:model, stock:stock})
+                .then(response => {
+                    console.log(response);
+                    this.listPhones();
+                });
+        },
+        // Eliminar
+        deletePhone: function(id) {
+            axios.post(url, {option: 3, id:id})
+                .then(response => {
+                    console.log(response);
+                    this.listPhones();
+                });
         }
     },
     created: function() {
         this.listPhones();
     },
-    computed: {}
+    computed: {
+        totalStock() {
+            this.total = 0;
+            for(phone of this.phones) {
+                this.total += parseInt(phone.stock);
+            }
+            return this.total;
+        }
+    }
 });
